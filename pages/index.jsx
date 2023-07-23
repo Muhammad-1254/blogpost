f1420ae4f0d9332df22d9490840b22b5fe67d01f
Muhammad usman <107724827+Muhammad-1254@users.noreply.github.com>
2023-07-23 09:06:38 +0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import {getPost} from '../service';
 import { FeaturedPost} from '@/sections';
 
 
-export const revalidate = 60*5;
+// export const revalidate = 60*5;
 
 export default function Home({posts}) {
   return (
